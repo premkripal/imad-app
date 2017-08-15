44fc6e811e61fc0797b87257d20191795adf8ec6
premkripal <30907885+premkripal@users.noreply.github.com>
2017-08-15 14:21:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one',function (req, res) {
+    res.send ("article1 will be serviced here");
+});
+app.get('/article-two',function (req, res) {
+    res.send ("article2 will be serviced here");
+});
+app.get('/article-three',function (req, res) {
+    res.send ("article3 will be serviced here");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
